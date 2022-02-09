6e4d05eb4cc6c22572b0173578db57916c31c336
unijreng <redomeire2@gmail.com>
2022-02-09 21:47:12 +0700

    resolving wrong path

--- src/Cards.js
+++ src/Cards.js
@@ -6,7 +6,7 @@ class Cards extends HTMLElement {
         this.innerHTML = `<div class="cards d-flex flex-wrap justify-content-center mt-4 px-4">
         <div class="card m-sm-1 p-4 border-0 d-flex flex-column align-items-center justify-content-center"
             style="width: 380px; border-radius: 20px;">
-            <img src="./src/assets/jaga-jarak.png" height="145" alt="...">
+            <img src="./assets/jaga-jarak.png" height="145" alt="...">
             <div class="card-body">
                 <h6 class="text-white fw-bold text-center mt-3">Jaga jarak anda</h6>
                 <p class="card-text text-center text-white mt-3">Selalu jaga jarak aman dari orang lain (minimal 1 meter) meskipun mereka tidak nampak sakit</p>
@@ -14,7 +14,7 @@ class Cards extends HTMLElement {
         </div>
         <div class="card m-sm-1 p-3 border-0 d-flex flex-column align-items-center justify-content-center"
             style="width: 380px; border-radius: 20px;">
-            <img src="./src/assets/hand-sanitizer.png" height="145" alt="...">
+            <img src="./assets/hand-sanitizer.png" height="145" alt="...">
             <div class="card-body">
                 <h6 class="text-white fw-bold text-center mt-3">Gunakan Hand Sanitizer</h6>
                 <p class="card-text text-center text-white mt-3">Setidaknya, hand sanitizer membantu mensterilkan tangan anda, namun lebih baik gunakan sabun</p>
@@ -22,7 +22,7 @@ class Cards extends HTMLElement {
         </div>
         <div class="card m-sm-1 p-3 border-0 d-flex flex-column align-items-center justify-content-center"
             style="width: 380px; border-radius: 20px;">
-            <img src="./src/assets/orang-batuk.png" height="145" alt="...">
+            <img src="./assets/orang-batuk.png" height="145" alt="...">
             <div class="card-body">
                 <h6 class="text-white fw-bold text-center mt-3">Tutup Mulut Saat Batuk</h6>
                 <p class="card-text text-center text-white mt-3">Batuk bisa menjadi langkah awal bagi seseorang menularkan penyakit kepada orang lain.</p>
